770c0d09ecf03fa6f3b9c33d9d13a9e65cf603d6
zhangchuang <keikeizhang540623@gmail.com>
2023-05-09 22:53:51 +0800

    修复若干问题

---
+++ static/js/useTargetData.hook-507ced02.js
@@ -0,0 +1,1 @@
+import{u as a}from"./chartEditStore-773be825.js";import{a0 as o}from"./index-db98ece3.js";const u=()=>{const t=a();return{targetData:o(()=>{const e=t.getComponentList,r=t.fetchTargetIndex();return e[r]}),chartEditStore:t}};export{u};
--- static/js/useTargetData.hook-6d85837e.js
+++
@@ -1,1 +0,0 @@
-import{u as a}from"./chartEditStore-a19f0a3f.js";import{a0 as o}from"./index-564e271e.js";const u=()=>{const t=a();return{targetData:o(()=>{const e=t.getComponentList,r=t.fetchTargetIndex();return e[r]}),chartEditStore:t}};export{u};
