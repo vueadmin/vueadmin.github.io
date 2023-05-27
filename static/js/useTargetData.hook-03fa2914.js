fcf10965d53763940c6c51cfa49f2dfb4e2aef16
JsonZhang <keikeizhang540623@gmail.com>
2023-05-27 11:20:35 +0800

    更新 2023-05-27

---
+++ static/js/useTargetData.hook-03fa2914.js
@@ -0,0 +1,1 @@
+import{u as a}from"./chartEditStore-287f703e.js";import{a0 as o}from"./index-3b318239.js";const u=()=>{const t=a();return{targetData:o(()=>{const e=t.getComponentList,r=t.fetchTargetIndex();return e[r]}),chartEditStore:t}};export{u};
