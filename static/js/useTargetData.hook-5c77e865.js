204bd5bed35972a7943a430e153dfd720141d751
JsonZhang <keikeizhang540623@gmail.com>
2023-05-30 23:10:19 +0800

    update: 20230530

---
+++ static/js/useTargetData.hook-5c77e865.js
@@ -0,0 +1,1 @@
+import{u as a}from"./chartEditStore-1f0ad4eb.js";import{a0 as o}from"./index-499f2953.js";const u=()=>{const t=a();return{targetData:o(()=>{const e=t.getComponentList,r=t.fetchTargetIndex();return e[r]}),chartEditStore:t}};export{u};
--- static/js/useTargetData.hook-7459931e.js
+++
@@ -1,1 +0,0 @@
-import{u as a}from"./chartEditStore-ee6fc9f5.js";import{a0 as o}from"./index-cf8681a3.js";const u=()=>{const t=a();return{targetData:o(()=>{const e=t.getComponentList,r=t.fetchTargetIndex();return e[r]}),chartEditStore:t}};export{u};
